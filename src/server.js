e6012df790f805f59d19ff7e8ff6d13450c20adf
Sean Johnson <seanjohnson220@gmail.com>
2020-12-15 13:46:29 -0500

    new commit

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 const knex = require('knex')
 const app = require('./app')
-const { PORT, DATABASE_URL } = require('./config')
+const { PORT, DATABASE_URL, NODE_ENV } = require('./config')
 
 const db = knex({
   client: 'pg',
-  connection: DATABASE_URL
+  connection: DATABASE_URL,
+  ssl: NODE_ENV === 'production'
 })
 
 app.set('db', db)
