d4db0ab42f0b7638f9477c0afe8e887a4558d872
Sean Johnson <seanjohnson220@gmail.com>
2020-12-15 10:08:32 -0500

    update error handler

--- src/error-handler.js
+++ src/error-handler.js
@@ -5,6 +5,7 @@ const { NODE_ENV } = require('./config')
 function errorHandler(error, req, res, next) {
     let response
     if (NODE_ENV === 'production') {
+        console.error(error)
         response = { error: { message: 'server error'} }
     }
     else {
